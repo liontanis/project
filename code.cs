57004d401a8a0ba12d6c2d4df781689ccb6569ff
Aien Liotta <161454969+liontanis@users.noreply.github.com>
2024-02-27 12:32:35 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,27 @@
+  public CurveKey this[int index]
+        {
+            get { return _keys[index]; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+
+                if (index >= _keys.Count)
+                    throw new IndexOutOfRangeException();
+
+                if (_keys[index].Position == value.Position)
+                    _keys[index] = value;
+                else
+                {
+                    _keys.RemoveAt(index);
+                    _keys.Add(value);
+                }
+            }
+        }
+
+  public class DrawableGameComponent : GameComponent, IDrawable
+    {
+        private bool _initialized;
+        private bool _disposed;
+        private int _drawOrder;
+        private bool _visible = true;
